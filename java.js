7f3db604cfb6be778d7dd9329d02f616ed4591ef
willokko <143515354+willokko@users.noreply.github.com>
2023-08-29 10:51:58 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+const mainButton = document.getElementById('mainButton');
+const cadastrarButton = document.getElementById('cadastrarButton');
+
+mainButton.addEventListener("click", function() {
+    window.location.href = "creation.html";
+  });
